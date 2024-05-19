6723055421c8631d625877473229e92905be4038
Kroscor <kroscor010@iesmartinezm.es>
2024-05-19 20:28:23 +0200

    Ejercicio 7 pruebas de ramas

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola aqui estamos haciendo pruebas con git");
+		System.out.println("Ejemplo ramas");
 	}
 }
