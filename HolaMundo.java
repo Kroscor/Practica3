9da30865fd6af53c22661a290623c6e17bd6e924
Kroscor <kroscor010@iesmartinezm.es>
2024-05-20 18:57:09 +0200

    Añadi holaaaaaa

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola aqui estamos haciendo pruebas con git");
 		System.out.println("Ejemplo ramas");
+		System.out.println("Holaaaaaaaaaaaaaaaaaaaaaaa");
 	}
 }
